4deac79332a243bd5d3c4cff97a31b42a545e3e3
David Ninh <Dninh12@gmail.com>
2021-02-08 23:40:27 -0600

    2/8 Sec 13 Fin

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -68,7 +69,6 @@ router.post('/',
         );    
           //return json webtoken
 
-        res.send('User Registered');
       } catch (err) {
           console.error(err.message);
           res.status(500).send('Server error');
